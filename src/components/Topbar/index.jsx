e559e069bf0e19ef83323159bfb11485a2244291
Akin Sel Coskun <akn_coskun@outlook.com>
2022-12-23 04:01:04 +0300

    fixed sidebar active class

--- src/components/Topbar/index.jsx
+++ src/components/Topbar/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import styles from './topbar.module.scss'
-import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
-
+import Avatar from '@mui/material/Avatar';
+import avatar from '../../assets/avatar.png'
 const Topbar = () => {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
@@ -14,10 +14,11 @@ const Topbar = () => {
   const handleClose = () => {
     setAnchorEl(null);
   };
+
   return (
     <div className={styles.topBarContainer}>
       <div className={styles.left}>
-        <MenuOutlinedIcon className={styles.hamburgerIcon}/>
+        <MenuOutlinedIcon className={styles.hamburgerIcon} />
       </div>
 
       <div className={styles.searchBar}>
@@ -26,24 +27,18 @@ const Topbar = () => {
 
       </div>
       <div className={styles.account}>
-        <Button
-          id="basic-button"
-          aria-controls={open ? 'basic-menu' : undefined}
-          aria-haspopup="true"
-          aria-expanded={open ? 'true' : undefined}
+        <Avatar
+          alt="Akin Sel Coskun"
+          src={avatar}
+          sx={{ width: 34, height: 34 }}
           onClick={handleClick}
-          variant={'outlined'}
-        >
-          Account
-        </Button>
+        />
+
         <Menu
           id="basic-menu"
           anchorEl={anchorEl}
           open={open}
           onClose={handleClose}
-          MenuListProps={{
-            'aria-labelledby': 'basic-button',
-          }}
         >
           <MenuItem onClick={handleClose}>Profile</MenuItem>
           <MenuItem onClick={handleClose}>Logout</MenuItem>
